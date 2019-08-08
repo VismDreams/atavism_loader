58f741390fe765662c5711238665027c4d8706ca
VismDreams <cerebralagony@gmail.com>
2019-08-08 03:12:16 +0300

    init commit

---
+++ loader.s
@@ -0,0 +1,24 @@
+                                        ; nasm -f elf32 loader.s
+
+global loader                           ; ELF - некая загрузочная программа для grub -> loader.elf
+
+    ATAVISM_LOAD    equ 0x1FAMB002      ; инициализация LOAD константы
+    FLAGS           equ 0x0             ; инициализация мультибута
+    CHECKSUM        equ -ATAVISM_LOAD   ; ATAVISM_LOAD + FLAGS + CHECKSUM = 0 !!!
+
+; Здесь стартует вся секция кода
+section .text:
+    align 4                             ; инициализация предела для кода/текста - 4 байта
+
+        ; подгрузка
+        dd ATAVISM_LOAD
+        dd FLAGS
+        dd CHECKSUM
+
+; Входная точка для линкера
+loader:
+    mov eax, ATAVISM_LOAD
+
+; Я хз как это работает
+.loop:
+    jmp .loop
